8a43413fd6779f681363867173703fa10a35c333
kempeter <kempeter33@gmail.com>
2025-11-07 09:33:08 +0100

    add problem 6

---
+++ src/problem006.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <math.h>
+
+int problem6() {
+    int sum1 = 0;
+    int sum2 = 0;
+
+    for (int i = 0; i <= 100; i++) {
+        sum1 += pow(i, 2);
+        sum2 += i;
+    }
+    sum2 = pow(sum2, 2);
+
+    return sum2 - sum1;
+}
+
+int main() {
+    int result = problem6();
+    std::cout << result << "\n";
+}
