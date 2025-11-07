aaff1badd2f1721869d3805a940932b2148895e9
kempeter <kempeter33@gmail.com>
2025-11-07 09:07:24 +0100

    update problem 1

--- src/problem001.cpp
+++ src/problem001.cpp
@@ -1,19 +1,26 @@
 #include <iostream>
 #include <vector>
 
-void problem1() {
+int problem1() {
 		std::vector<int> nums;
 		int sum = 0;
 		for (int i = 0; i < 1000; i++) {
 
-				if (i % 3 == 0 || i % 5 == 0) {
-						nums.push_back(i);
-				}
+            if (i % 3 == 0 || i % 5 == 0) {
+                    nums.push_back(i);
+            }
 
 		}
 
 		for (const int& i : nums) {
 				sum += i;
 		}
-		std::cout << sum;
+		
+        return sum;
     }
+
+int main() {
+    int sum = problem1();
+
+    std::cout << sum << "\n";
+}
