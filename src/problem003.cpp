9c06c8c85a29e03d92437b15720b0fa1686462a1
kempeter <kempeter33@gmail.com>
2025-11-07 09:16:33 +0100

    add problem 3

---
+++ src/problem003.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stdint.h>
+
+uint64_t problem3() {
+    uint64_t num = 600851475143;
+    uint64_t divisor = 1;
+
+    while (num != 1) {
+
+            divisor += 2;
+            while (num % divisor == 0) {
+                    num /= divisor;
+            }
+    }
+    return divisor;
+}
+
+int main() {
+    uint64_t divisor = problem3();
+    std::cout << divisor << "\n";
+}
